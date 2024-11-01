f01e0de008b8ac5c309ae3e7f38e0e9a51547805
Ridwan Nuwllah <chikofrainstact097@gmail.com>
2024-11-01 12:10:57 +0700

    add struktur getx

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bukuku_mart/main.dart';
+import 'package:bukuku_mart/module/Main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
